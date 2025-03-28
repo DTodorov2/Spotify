298a4fbe7bcc4a8ac5bcd4a90174a96b705e2d37
Danail Todorov <danailtodorov440@gmail.com>
2025-03-28 16:45:10 +0200

    Delete Spotify/src/bg/sofia/uni/fmi/mjt/spotify/server/repository directory

--- Spotify/src/bg/sofia/uni/fmi/mjt/spotify/server/repository/serializable/SerializableRepository.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.spotify.server.repository.serializable;
-
-import java.nio.file.Path;
-
-public interface SerializableRepository<T> {
-    void loadDataFrom(Path filePath);
-
-    void saveDataTo(Path filePath);
-}
