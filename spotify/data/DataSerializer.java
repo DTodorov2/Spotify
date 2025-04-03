11849227b206771a177eda0360d69dc65f32bd38
Danail Todorov <danailtodorov440@gmail.com>
2025-04-03 20:51:15 +0300

    Delete spotify directory

--- spotify/data/DataSerializer.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.spotify.data;
-
-import bg.sofia.uni.fmi.mjt.spotify.exception.checked.SerializationException;
-
-public interface DataSerializer {
-    void saveData(String data, boolean toAppend) throws SerializationException;
-}
--- spotify/server/model/IdentifiableModel.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.spotify.server.model;
-
-import java.util.Map;
-
-public abstract class IdentifiableModel<T> {
-    public abstract T getId();
-
-    public abstract boolean isDuplicate(Map<String, ? extends IdentifiableModel<T>> entities);
-}
