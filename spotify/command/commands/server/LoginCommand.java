c1b095a03d5cd28b75061ca5d100630478ff87c1
Danail Todorov <danailtodorov440@gmail.com>
2025-04-04 00:19:12 +0300

    Add files via upload

---
+++ spotify/command/commands/server/LoginCommand.java
@@ -0,0 +1,34 @@
+package bg.sofia.uni.fmi.mjt.spotify.command.commands.server;
+
+import bg.sofia.uni.fmi.mjt.spotify.server.model.User;
+import bg.sofia.uni.fmi.mjt.spotify.server.resources.ServerResources;
+import bg.sofia.uni.fmi.mjt.spotify.server.security.SHA256;
+
+public class LoginCommand extends ServerCommand {
+
+    private static final int EXPECTED_INPUT_LENGTH = 3;
+
+    public LoginCommand(String[] arguments, ServerResources serverResources) {
+        super(arguments, EXPECTED_INPUT_LENGTH, serverResources);
+    }
+
+    @Override
+    public String execute() {
+        String userEmail = arguments[0];
+        String userPass = arguments[1];
+
+        User currUser = serverResources.getUsersRepository().getMap().get(userEmail);
+
+        if (currUser == null) {
+            return "No such user!";
+        }
+
+        if (!currUser.getPassword().equals(SHA256.hash(userPass))) {
+            return "Incorrect password!";
+        }
+
+        currUser.setLoggedIn(true);
+        serverResources.getSelectionKey().attach(currUser.getId()); // za da mojem da razlichavame potrebitelite
+        return "The user is successfully logged in";
+    }
+}
