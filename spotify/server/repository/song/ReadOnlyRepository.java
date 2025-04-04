ae5254b81d0be792afaab1ad8d99c92f9c8815db
Danail Todorov <danailtodorov440@gmail.com>
2025-04-04 13:10:42 +0300

    Add files via upload

---
+++ spotify/server/repository/song/ReadOnlyRepository.java
@@ -0,0 +1,13 @@
+package bg.sofia.uni.fmi.mjt.spotify.server.repository.song;
+
+import bg.sofia.uni.fmi.mjt.spotify.server.model.Song;
+import java.util.List;
+
+public interface ReadOnlyRepository {
+
+    List<Song> searchByWords(String... words);
+
+    List<Song> getTopStatistics(int num);
+
+    List<Song> getSongByName(String songName);
+}
