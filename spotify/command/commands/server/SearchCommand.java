2814f76220d223b0c44b599f9effaafc5e217bc3
Danail Todorov <danailtodorov440@gmail.com>
2025-04-04 15:26:08 +0300

    Add files via upload

---
+++ spotify/command/commands/server/SearchCommand.java
@@ -0,0 +1,23 @@
+package bg.sofia.uni.fmi.mjt.spotify.command.commands.server;
+
+import bg.sofia.uni.fmi.mjt.spotify.server.resources.ServerResources;
+
+public class SearchCommand extends ServerCommand {
+
+    private static final int EXPECTED_INPUT_LENGTH = 1;
+
+    public SearchCommand(String[] arguments, ServerResources serverResources) {
+        super(arguments, EXPECTED_INPUT_LENGTH, serverResources);
+    }
+
+    @Override
+    public String execute() {
+        boolean isUserLogged = isUserLogged();
+        if (!isUserLogged) {
+            return "You must be logged in in order to use the \\search\\ option";
+        }
+
+        return serverResources.getSongsRepository().searchByWords(arguments).toString();
+    }
+
+}
