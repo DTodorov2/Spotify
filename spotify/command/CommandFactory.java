2c64b9d32411eca5d5ecbccce51f2b9f18dfbbbf
Danail Todorov <danailtodorov440@gmail.com>
2025-04-06 19:46:35 +0300

    Delete spotify directory

--- spotify/command/CommandFactory.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.spotify.command;
-
-//това е функционален интерфейс, който ни дава възможността в мапа с команди да слагаме ламнбда функции
-import bg.sofia.uni.fmi.mjt.spotify.command.commands.Command;
-
-public interface CommandFactory {
-    Command create(String[] args);
-}
