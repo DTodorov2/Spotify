82998f2be639d96026762b67f9fd805216d2c16f
Danail Todorov <danailtodorov440@gmail.com>
2025-04-09 15:22:08 +0300

    Update SpotifyClient.java

--- spotify/client/SpotifyClient.java
+++ spotify/client/SpotifyClient.java
@@ -8,19 +8,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
 import java.util.Scanner;
-package bg.sofia.uni.fmi.mjt.spotify.client;
-
-import bg.sofia.uni.fmi.mjt.spotify.client.resources.ClientResources;
-import bg.sofia.uni.fmi.mjt.spotify.command.CommandExecutor;
-import bg.sofia.uni.fmi.mjt.spotify.exception.checked.ChannelCommunicationException;
-
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.channels.SocketChannel;
-import java.util.Scanner;
-
-import static bg.sofia.uni.fmi.mjt.spotify.validator.Validator.validateInt;
-import static bg.sofia.uni.fmi.mjt.spotify.validator.Validator.validateString;
 
 public class SpotifyClient {
 
@@ -47,95 +34,28 @@ public class SpotifyClient {
         }
     }
 
+    // tuk dobavih break-ove i zamestih isClientRunning s true
     private void runClientLoop() {
-        boolean isClientRunning = true;
+        //boolean isClientRunning = true;
         Scanner scanner = new Scanner(System.in);
 
-        while (isClientRunning) {
+        while (true) {
             System.out.println("Enter command: ");
             String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("disconnect")) {
-                isClientRunning = false;
-                //tuka trqq proverqm, ako puska muzika da q spiram
-                clientResources.setClientLogged(false);
-                continue;
-            }
+
             try {
                 String response = commandExecutor.execute(input);
+                System.out.println(response);
+
             } catch (ChannelCommunicationException e) {
                 System.out.println("Unable to connect with the server! " + e.getMessage() +
                         System.lineSeparator() + " Exiting...");
 
-                isClientRunning = false;
                 if (clientResources.isClientLogged()) {
-                    //ako stream-va muziak da spira
+                    //ako stream-va muzika da spira
                 }
+                break;
             }
         }
     }
 }
-
-
-
-import static bg.sofia.uni.fmi.mjt.spotify.validator.Validator.validateInt;
-import static bg.sofia.uni.fmi.mjt.spotify.validator.Validator.validateString;
-
-public class SpotifyClient {
-
-    private final String serverHost;
-    private final int serverPort;
-
-    private final ClientResources clientResources;
-
-    private final CommandExecutor commandExecutor;
-
-    public SpotifyClient(String serverHost, int serverPort) {
-        validateString(serverHost, "server host");
-        validateInt(serverPort, "server port");
-
-        this.serverHost = serverHost;
-        this.serverPort = serverPort;
-
-        clientResources = new ClientResources();
-        commandExecutor = new CommandExecutor(clientResources);
-    }
-
-    private void start() {
-        try (SocketChannel clientChannel = SocketChannel.open()) {
-            clientChannel.connect(new InetSocketAddress(serverHost, serverPort));
-            this.clientResources.setClientChannel(clientChannel);
-            runClientLoop();
-        } catch (IOException e) {
-            System.out.println("Unable to connect to the server!");
-        }
-    }
-
-    private void runClientLoop() {
-        boolean isClientRunning = true;
-        Scanner scanner = new Scanner(System.in);
-
-        while (isClientRunning) {
-            System.out.println("Enter command: ");
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("disconnect")) {
-                isClientRunning = false;
-                //tuka trqq proverqm, ako puska muzika da q spiram
-                clientResources.setClientLogged(false);
-                continue;
-            }
-            try {
-                String response = commandExecutor.execute(input);
-            } catch (ChannelCommunicationException e) {
-                System.out.println("Unable to connect with the server! " + e.getMessage() +
-                        System.lineSeparator() + " Exiting...");
-
-                isClientRunning = false;
-                if (clientResources.isClientLogged()) {
-                    //ako stream-va muziak da spira
-                }
-            }
-        }
-    }
-}
-
-
