58899e27acfc1a4e564415c0c66f363c3d8f124a
Danail Todorov <danailtodorov440@gmail.com>
2025-04-17 00:23:21 +0300

    Delete spotify directory

--- spotify/launch/StartServer.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.spotify.launch;
-
-import bg.sofia.uni.fmi.mjt.spotify.server.SpotifyServer;
-
-public class StartServer {
-    public static void main(String[] args) {
-        new SpotifyServer().start();
-    }
-}
