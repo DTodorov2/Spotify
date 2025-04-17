2ce9a6b79e418bdb81359dc8ebf047ed799f714e
Danail Todorov <danailtodorov440@gmail.com>
2025-04-17 16:21:43 +0300

    Delete spotify directory

--- spotify/command/commands/client/ClientCommandFactory.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.spotify.command.commands.client;
-
-import bg.sofia.uni.fmi.mjt.spotify.command.commands.Command;
-
-public interface ClientCommandFactory {
-    Command create(String userInput);
-}
